4e67b36a8dfb067b84648ac78367bf76cc3dfd06
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-02-08 15:16:13 +0900

    18 feat: user_data에 lifecycle 완료 로직 추가

--- component/launch-template.tf
+++ component/launch-template.tf
@@ -2,6 +2,11 @@ resource "aws_launch_template" "app" {
   name_prefix   = "${var.environment}-app-"
   image_id      = coalesce(var.app_ami_id, data.aws_ami.app.id)
   instance_type = "t3.micro"
+  user_data = base64encode(templatefile("${path.module}/user-data/app-user-data.sh.tmpl", {
+    asg_name            = var.app_asg_name
+    lifecycle_hook_name = var.app_lifecycle_hook_name
+    aws_region          = var.aws_region
+  }))
 
   iam_instance_profile {
     name = aws_iam_instance_profile.ec2.name
