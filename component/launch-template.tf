268442e69b0991bb9fc66ea5ece03f3ec5bf09b3
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-02-11 21:25:00 +0900

    21 feat: EC2 인스턴스 타입을 t3.small로 상향

--- component/launch-template.tf
+++ component/launch-template.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "app" {
   name_prefix   = "${var.environment}-app-"
   image_id      = coalesce(var.app_ami_id, data.aws_ami.app.id)
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   user_data = base64encode(templatefile("${path.module}/user-data/app-user-data.sh.tmpl", {
     asg_name            = var.app_asg_name
     lifecycle_hook_name = var.app_lifecycle_hook_name
